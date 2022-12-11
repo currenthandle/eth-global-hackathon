d5ec1a6459a35564d9d0e3334176fe1cc60ca25a
Casey <leptone55@gmail.com>
2022-12-10 22:28:09 -0800

    useMutation not throwing error

--- components/Sample.tsx
+++ components/Sample.tsx
@@ -1,4 +1,4 @@
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, useMutation, gql } from '@apollo/client';
 
 const allUsers = gql`
   query AllUsers {
@@ -7,13 +7,30 @@ const allUsers = gql`
     }
   }
 `;
+const createUser = gql`
+  mutation CreateUser($email: String!, $password: String!) {
+    createUser(email: $email, password: $password) {
+      email
+    }
+  }
+`;
+
 export const Sample = () => {
-  const { loading, error, data } = useQuery(allUsers);
+  // const { loading, error, data } = useQuery(allUsers);
+  const [createUserMutation] = useMutation(createUser);
+
   return (
-    <div>
-      {data.allUsers.map((user) => {
-        return <div key={user.email}>{user.email}</div>;
-      })}
+    <div
+      onClick={async () => {
+        createUserMutation({
+          variables: {
+            email: 'askdkfjahl@als.com',
+            password: '123456',
+          },
+        });
+      }}
+    >
+      hello
     </div>
   );
 };
