4397af6932d4baa03bdbb0d22d3857a30f3672fc
Casey <leptone55@gmail.com>
2022-12-10 23:37:12 -0800

    trying to config tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
 import client from '../apollo-client';
 import { SessionProvider } from 'next-auth/react';
+
+import '../styles/globals.css';
 
 export default function App({
   Component,
