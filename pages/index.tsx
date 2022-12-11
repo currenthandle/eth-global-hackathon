5d83750174c3fac2d42e1a24c415718f270476db
Casey <leptone55@gmail.com>
2022-12-10 23:52:42 -0800

    redirect w/o session is working

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,12 @@ import Image from 'next/image';
 import ClientOnly from '../components/ClientOnly';
 import { Sample } from '../components/Sample';
 // import styles from '../styles/Home.module.css';
+import {
+  type GetServerSideProps,
+  type GetServerSidePropsContext,
+  type NextPage,
+} from 'next';
+import { getServerAuthSession } from '../get-server-auth-session';
 
 export default function Home() {
   return (
@@ -14,3 +20,26 @@ export default function Home() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+) => {
+  const session = await getServerAuthSession(ctx);
+
+  console.log('session', session);
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+      props: {},
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
