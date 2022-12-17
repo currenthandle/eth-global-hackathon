7663f1df91bb7c9feba450fdd034fec758a812c3
Casey <leptone55@gmail.com>
2022-12-16 16:58:57 -0800

    basic partner UI

--- client/components/PartnerEditor.tsx
+++ client/components/PartnerEditor.tsx
@@ -9,36 +9,12 @@ const schema = z.object({
   firstName: z.string().optional(),
   lastName: z.string().optional(),
   website: z.string().optional(),
-  github: z.string().optional(),
+  organization: z.string().optional(),
   linkedin: z.string().optional(),
-  yearsOfExp: z
-    .number()
-    .or(z.string().regex(/\d+/).transform(Number))
-    .refine((n) => n >= 0),
-  // z.union([z.literal(0), z.string().numeric().min(1)]),
-  ethExp: z.union([
-    z.literal('beginner'),
-    z.literal('intermediate'),
-    z.literal('advanced'),
-    z.literal(''),
-  ]),
-  motivation: z.union([
-    z.literal(''),
-    z.array(
-      z.union([
-        z.literal('workshop'),
-        z.literal('resume'),
-        z.literal('improve'),
-        z.literal('jobOps'),
-        z.literal('meetPpl'),
-        z.literal('launchProduct'),
-        z.literal('winPrize'),
-        z.literal('other'),
-      ])
-    ),
-  ]),
-  builtBefore: z.union([z.string().min(250).max(500), z.literal('')]),
-  lookingToBuild: z.union([z.string().min(250).max(500), z.literal('')]),
+  telegram: z.string().optional(),
+  twitter: z.string().optional(),
+  otherEvents: z.union([z.string().min(250).max(500), z.literal('')]),
+  reasonForSupporting: z.union([z.string().min(250).max(500), z.literal('')]),
   rules: z.boolean().default(false),
   // website: z.union([z.string(), z.literal('')]),
 });
@@ -58,13 +34,12 @@ const PartnerEditor = ({ userSsr }) => {
       firstName: userSsr.firstName || '',
       lastName: userSsr.lastName || '',
       website: userSsr.website || '',
-      github: userSsr.github || '',
+      organization: userSsr.organization || '',
       linkedin: userSsr.linkedin || '',
-      yearsOfExp: userSsr.yearsOfExp || 0,
-      ethExp: userSsr.ethExp || '',
-      motivation: userSsr.motivation || '',
-      builtBefore: userSsr.builtBefore || '',
-      lookingToBuild: userSsr.lookingToBuild || '',
+      telegram: userSsr.telegram || '',
+      twitter: userSsr.twitter || '',
+      otherEvents: userSsr.otherEvents || '',
+      reasonForSupporting: userSsr.reasonForSupporting || '',
       rules: userSsr.rules || false,
     },
   });
@@ -83,20 +58,16 @@ const PartnerEditor = ({ userSsr }) => {
           lastName: formValues.lastName || '',
         },
         partnerProfile: {
-          github: formValues.github || '',
-          linkedin: formValues.linkedin || '',
           website: formValues.website || '',
-          yearsOfExp: Number(formValues.yearsOfExp) || 0,
-          ethExp: formValues.ethExp,
-          motivation: formValues.motivation || '',
-          builtBefore: formValues.builtBefore || '',
-          lookingToBuild: formValues.lookingToBuild || '',
+          organization: formValues.organization || '',
+          linkedin: formValues.linkedin || '',
+          telegram: formValues.telegram || '',
+          twitter: formValues.twitter || '',
+          otherEvents: formValues.otherEvents || '',
+          reasonForSupporting: formValues.reasonForSupporting || '',
           rules: formValues.rules || false,
         },
       };
-      console.log('formValues', formValues);
-      console.log('submit variables', variables);
-
       const updatedUser = await updateUser({
         variables,
       });
@@ -105,8 +76,6 @@ const PartnerEditor = ({ userSsr }) => {
     }
   };
 
-  // console.log('formVale', getValues().yearsOfExp);
-  // console.log('formVale', typeof getValues().yearsOfExp);
   return (
     <div className='flex justify-center'>
       <div className='flex justify-center flex-col items-center'>
@@ -151,14 +120,15 @@ const PartnerEditor = ({ userSsr }) => {
             id='website'
             placeholder='Website'
           />
-          <label htmlFor='github'>Github</label>
+
+          <label htmlFor='github'>Organization</label>
           <input
             className='rounded-md border-2 border-[#dadadb] mb-6 h-12'
-            {...register('github')}
+            {...register('organization')}
             type='text'
-            name='github'
-            id='github'
-            placeholder='Github'
+            name='organization'
+            id='organization'
+            placeholder='Organization'
           />
 
           <label htmlFor='linkedin'>Linkedin</label>
@@ -170,82 +140,49 @@ const PartnerEditor = ({ userSsr }) => {
             id='linkedin'
             placeholder='Linkedin'
           />
-          <label htmlFor='yearsOfExp'>
-            How many years of experience do you have with sofware development?
-          </label>
+
+          <label htmlFor='telegram'>Telegram</label>
           <input
             className='rounded-md border-2 border-[#dadadb] mb-6 h-12'
-            {...register('yearsOfExp')}
-            type='number'
-            name='yearsOfExp'
-            id='yearsOfExp'
-            placeholder='Year of experience'
-            min='0'
+            {...register('telegram')}
+            type='text'
+            name='telegram'
+            id='telegram'
+            placeholder='Telegram'
           />
-          <label htmlFor='yearsOfExp'>
-            {"What's your experience level with Ethereum?"}
-          </label>
-          <select
+
+          <label htmlFor='twitter'>Twitter</label>
+          <input
             className='rounded-md border-2 border-[#dadadb] mb-6 h-12'
-            {...register('ethExp')}
-            name='ethExp'
-            id='ethExp'
-            placeholder='beginner'
-          >
-            <option value='beginner'>Beginner</option>
-            <option value='intermediate'>Intermediate</option>
-            <option value='beginner'>Beginner</option>
-          </select>
-          <label htmlFor='yearsOfExp'>
-            {"What's your experience level with Ethereum?"}
-          </label>
-          <select
-            className='rounded-md border-2 border-[#dadadb] mb-6 h-12'
-            {...register('motivation')}
-            name='motivation'
-            id='motivation'
-            placeholder='beginner'
-            defaultValue={['workshop']}
-            multiple
-          >
-            <option value='workshop'>Attend workshops & tech talks</option>
-            <option value='resume'>
-              Build something to put on my resume / portfolio
-            </option>
-            <option value='improve'>
-              Get better as a developer / designer
-            </option>
-            <option value='jobOps'>
-              Meet companies for a potential job opportunity
-            </option>
-            <option value='meetPpl'>
-              Meet like-minded people and make friends
-            </option>
-            <option value='launchProduct'>Launch a product</option>
-            <option value='winPrize'>Win a prize</option>
-            <option value='other'>Other</option>
-          </select>
-          <label htmlFor='builtBefore'>
-            {"Tell us about what you've built before?"}
+            {...register('twitter')}
+            type='text'
+            name='twitter'
+            id='twitter'
+            placeholder='Twitter'
+          />
+
+          <label htmlFor='otherEvents'>
+            Have you supported other Ethereum events before? If yes, which ones?
           </label>
           <textarea
             className='rounded-md border-2 border-[#dadadb] mb-6 h-12'
-            {...register('builtBefore')}
-            // type='text'
-            name='builtBefore'
-            id='builtBefore'
-            placeholder='builtBefore'
+            {...register('otherEvents')}
+            name='otherEvents'
+            id='otherEvents'
+            placeholder='Other Events'
           />
-          <label htmlFor='lookingToBuild'>
-            Tell us about what are you looking to build at this hackathon?
+
+          <label htmlFor='reasonForSupporting'>
+            Why do you want to support this hackathon?
           </label>
           <textarea
             className='rounded-md border-2 border-[#dadadb] mb-6 h-12'
-            {...register('lookingToBuild')}
-            name='lookingToBuild'
-            id='lookingToBuild'
-            placeholder='Looking to build'
+            {...register('reasonForSupporting')}
+            name='reasonForSupporting'
+            id='reasonForSupporting'
+            placeholder='Reason For Supporting'
           />
+
           <label htmlFor='rules'>
             Do you accept the rules and code of conduct for the event?
           </label>
