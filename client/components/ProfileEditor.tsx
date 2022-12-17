b4a7e39ae1d0a25adefa51916b5d89012d12d6cd
Casey <leptone55@gmail.com>
2022-12-16 16:47:39 -0800

    fix typo

--- client/components/ProfileEditor.tsx
+++ client/components/ProfileEditor.tsx
@@ -9,7 +9,7 @@ const ProfileEditor = ({ userSsr }) => {
     <div className='border-2 rounded-3xl border-[#dadadb] bg-white p-8 w-8/12 max-w-2xl m-auto '>
       {/* <Image src='/team-header.webp' alt='Team Header' fill></Image> */}
       {userSsr.role === 'hacker' && <HackerEditor userSsr={userSsr} />}
-      {userSsr.role === 'patrner' && <PartnerEditor userSsr={userSsr} />}
+      {userSsr.role === 'partner' && <PartnerEditor userSsr={userSsr} />}
     </div>
   );
 };
