3a234e1cf2283230ad29ed6ebc730078aee60e0a
Casey <leptone55@gmail.com>
2022-12-17 01:34:34 -0800

    resolve jwt type error

--- server/src/utils/getTokenPayload.ts
+++ server/src/utils/getTokenPayload.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { TokenPayload } from './types';
 
 function getTokenPayload(token: string): TokenPayload {
-  return jwt.verify(token, process.env.JWT_SECRET) as TokenPayload;
+  return jwt.verify(token, process.env.JWT_SECRET as string) as TokenPayload;
 }
 
 export default getTokenPayload;
